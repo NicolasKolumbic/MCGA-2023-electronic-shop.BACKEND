a35139789cde69fd2cda71eb80337e91203d0225
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2023-11-27 20:22:52 -0300

    install mongoose, class-validator and class transfom

--- web-api/src/app.module.ts
+++ web-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb+srv://nicolaskolumbic:universidad2023@clusteruniversidad.sxtgdsz.mongodb.net/electronic-shop'),
     ProductModule,
     CategoryModule
   ],
--- web-api/src/product/entities/product.entity.ts
+++ web-api/src/product/entities/product.entity.ts
@@ -1,2 +1,35 @@
+import { Schema, SchemaFactory, Prop } from "@nestjs/mongoose";
+import mongoose, { Document } from "mongoose";
+import { Category } from "src/category/entities/category.entity";
+
 // Entidades relacionadas con la BASE DE DATOS, cada una representa una COLLECION de la Base MongoDB 
-export class Product {}
+@Schema()
+export class Product extends Document{ 
+
+    @Prop({
+       isRequired: true,
+       minlength: 5,
+       type: String
+    })
+    description: string;
+
+    @Prop({
+        isRequired: true,
+        type: mongoose.Types.Decimal128
+    })
+    price: number;
+
+    @Prop({
+        isRequired: true,
+        isInteger: true
+    })
+    stock: number;
+
+    @Prop({
+        type: mongoose.Types.ObjectId,
+        ref: 'Category' 
+    })
+    category: Category
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
